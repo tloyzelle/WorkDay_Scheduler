aecbfb2b05fbd1c8f4b75dab2fbc0ea5d1afcbf3
Taegan Loyzelle <tloyzelle@gmail.com>
2021-06-22 20:42:31 -0400

    specifing certain areas

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ $(document).ready(function () {
         })
     }
 
-    // Get item from local storage if any
+    // Get item from local storage of each time if any
     $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
